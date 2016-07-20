448b2467bcc7993bce6eed95ddea1db8d891c1be
PinoNilo <naty205@gmail.com>
2016-07-20 09:17:05 -0400

    arreglo final de archivo js

---
+++ js/data.js
@@ -0,0 +1,38 @@
+var data = [
+  {
+    "nombre" : "Laboratoria Perú",
+    "hora" : "14:25",
+    "avatar" : "image/logocodeacademy.png",
+    "mensaje": "Dale dale!!",
+    "mensajes" : [
+      {
+        "texto" : "Cuando vamos al workshop?",
+        "hora" : "11:12"
+      },
+      {
+        "texto" : "Les parece el sábado?",
+        "hora" : "11:13"
+      },
+      {
+        "texto" : "Es la única opción >.<",
+        "hora" : "11:16"
+      },
+      {
+        "texto" : "Dale dale!!",
+        "hora" : "14:25"
+      }
+    ]
+  },
+  {
+    "nombre" : "Raymi Saldomando",
+    "hora" : "11:25",
+    "avatar" : "image/raymi.jpg",
+    "mensaje": "La clase va bien!!",
+    "mensajes" : [
+      {
+        "texto" : "La clase va bien!!",
+        "hora" : "11:25"
+      }
+    ]
+  }
+];
